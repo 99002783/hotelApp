23c5262d5f04123365944b6a70bf66ec89345939
99002783 <72496295+99002783@users.noreply.github.com>
2020-10-22 17:54:33 +0530

    Add files via upload

---
+++ HotelApp/src/com/hotelapp/dao/HotelDAOImpl.java
@@ -0,0 +1,33 @@
+package com.hotelapp.dao;
+
+import java.util.List;
+
+import com.hotelapp.model.Hotel;
+
+public class HotelDAOImpl implements HotelDAO {
+
+	@Override
+	public List<Hotel> getAllHotels() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Hotel> getByCity(String city) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Hotel> getByCuisine(String cuisine) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Hotel getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
