3edb247b3ecd530e065d0a67d04603032314f9a2
99002783 <72496295+99002783@users.noreply.github.com>
2020-10-22 17:58:21 +0530

    Add files via upload

---
+++ SpringRestAPI/src/main/java/com/example/controller/GreetController.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.GreetService;
+
+@RestController
+public class GreetController {
+	@Autowired
+	GreetService greetService;
+
+	@RequestMapping("/greet")
+	public String SayHello()
+	{
+		return greetService.ShowMessage();
+	}
+	
+	@GetMapping("/welcome/{username}")
+	public String sayWelcome(@PathVariable("username")String name)
+	{
+		return greetService.welcomeUser(name);
+		
+	}
+	@RequestMapping("/hi")
+	public String sayHi(@RequestParam("name")String name)
+	{
+		return greetService.printName(name);
+		
+	}
+	
+	
+}
---
+++ SpringRestAPI/src/main/java/com/example/service/GreetService.java
@@ -0,0 +1,8 @@
+package com.example.service;
+
+
+public interface GreetService {
+	String ShowMessage();
+	String printName(String name);
+	String welcomeUser(String name);
+}
